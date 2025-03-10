181c4fa585cf330be48c9565f6fc46d6c67fda87
Farheen112004 <farheenfairy2004@gmail>
2025-03-10 21:36:39 +0530

    nect commit

--- conditional statements/added_tax.py
+++ conditional statements/added_tax.py
@@ -1,5 +1,8 @@
-price = float(input('how much u paid?'))
+price = float(input('how much u paid? : '))
 
 if price>=1.00:
     tax= .07
-    print('the tax rate'+ str(tax))
+    print('the tax rate = '+ str(tax))
+else:
+    tax= 0
+    print('the tax rate = '+ str(tax))
