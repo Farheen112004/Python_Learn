d6cccf3f8689148f0402849eaa874e4e9cba9f21
Farheen112004 <farheenfairy2004@gmail>
2025-04-02 11:30:29 +0530

    dict

---
+++ collections/dict.py
@@ -0,0 +1,13 @@
+Farheen={}
+Farheen['first'] = 'Farheen'
+Farheen['last'] = 'syed'
+
+susan={}
+susan['first'] = 'susan'
+susan['last'] = 'mathews'
+
+people = []
+people.append(Farheen)
+people.append(susan)
+people.append({'first':'Bill','Last':'gates'})
+print(people)
